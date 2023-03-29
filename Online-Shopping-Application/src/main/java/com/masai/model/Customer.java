ae1f21e420ac65644e84cfd0a3e1af1cc9be44c8
Kumar Vishal Anand <vishalanand270@gmail.com>
2023-03-29 22:38:12 +0530

    day-2 done

--- Online-Shopping-Application/src/main/java/com/masai/model/Customer.java
+++ Online-Shopping-Application/src/main/java/com/masai/model/Customer.java
@@ -1,10 +1,16 @@
 package com.masai.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -51,6 +57,21 @@ public class Customer {
 	@NotBlank
 	@Email
 	private String email;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Order> listOfOrders = new ArrayList<>();
+
+	@JsonIgnore
+	public List<Order> getListOfOrders() {
+		return listOfOrders;
+	}
+
+	@JsonIgnore
+	public void setListOfOrders(List<Order> listOfOrders) {
+		this.listOfOrders = listOfOrders;
+	}
+	
+	
 	
 	
 }
