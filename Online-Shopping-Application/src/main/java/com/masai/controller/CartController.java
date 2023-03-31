9b7ef428fa1d25f98102258692e34efa3b748884
mdhumairraza <mdhumairraza@yahoo.com>
2023-03-31 13:43:24 +0530

    day-4

--- Online-Shopping-Application/src/main/java/com/masai/controller/CartController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/CartController.java
@@ -1,9 +1,16 @@
 package com.masai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.masai.model.Cart;
+import com.masai.model.Product;
 import com.masai.service.CartService;
 
 @RestController
@@ -14,7 +21,12 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-
+    
+    @PostMapping("/{quantity}")
+    public ResponseEntity<Cart> addProductToCartHandler(@RequestBody Cart cart,@RequestBody Product product,@RequestParam("quantity") int quantity){
+        Cart addProduct = cartService.addProductToCart(cart, product, quantity);
+        return new ResponseEntity<>(addProduct,HttpStatus.CREATED);
+    }
 
 
 
