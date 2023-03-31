2d47e4de410035612c3f747942dc6e4e095a422c
mdhumairraza <mdhumairraza@yahoo.com>
2023-03-31 13:37:56 +0530

    day-4

--- Online-Shopping-Application/src/main/java/com/masai/controller/CartController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/CartController.java
@@ -1,12 +1,21 @@
 package com.masai.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.service.CartService;
 
 @RestController
 @RequestMapping("/cart")
 public class CartController {
 
-	
-	
+
+    @Autowired
+    private CartService cartService;
+
+
+
+
+
 }
