f8ffe9dce2bf84789a7e3fb605a587e9d9902407
mdhumairraza <mdhumairraza@yahoo.com>
2023-03-31 13:52:32 +0530

    day-4

--- Online-Shopping-Application/src/main/java/com/masai/controller/CartController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/CartController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PatchExchange;
 
 import com.masai.model.Cart;
 import com.masai.model.Product;
@@ -33,6 +36,12 @@ public class CartController {
     public ResponseEntity<Cart> removeProductFromCartHandler(@RequestBody Cart cart,@RequestBody Product product){
         Cart removeProduct = cartService.removeProductFromCart(cart, product);
         return new ResponseEntity<>(removeProduct,HttpStatus.OK);
+    }
+
+    @PatchMapping("/{quantity}")
+    public ResponseEntity<Cart> updateProductQuantityHandler(@RequestBody Cart cart,@RequestBody Product product,@RequestParam("quantity") int quantity){
+        Cart updateProduct = cartService.updateProductQuantity(cart, product, quantity);
+        return new ResponseEntity<>(updateProduct,HttpStatus.ACCEPTED);
     }
 
 
