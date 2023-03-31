368b1de7cbe7ee317f705aa7dc532df8a1588069
mdhumairraza <mdhumairraza@yahoo.com>
2023-03-31 09:17:56 +0530

    day-4

--- Online-Shopping-Application/src/main/java/com/masai/controller/AddressController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/AddressController.java
@@ -1,11 +1,29 @@
 package com.masai.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.model.Address;
+import com.masai.service.AddressService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/addresses")
 public class AddressController {
 	
+	@Autowired
+	private AddressService addressService;
+	
+	@PostMapping
+	public ResponseEntity<Address> addAddressHandler(@Valid @RequestBody Address add) {
+		Address ad = addressService.addAddress(add);
+		return new ResponseEntity<Address>(ad, HttpStatus.OK);
+	}
 	
 }
