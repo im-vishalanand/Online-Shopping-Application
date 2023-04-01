56c4da2a0fabd5f040d200f1182a5788544fe86c
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 14:40:03 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
@@ -6,18 +6,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.masai.service.OrderService;
 
 @RestController
+@RequestMapping("/orders")
 public class orderController {
 
 	@Autowired
     private OrderService orderService;
 	
 	
-	@PostMapping("/orders/setOrder")
+	@PostMapping
 	 public ResponseEntity<Order> addOrderHandler(@RequestBody Order order){
 		 
 		 Order addedOrder = orderService.addOrder(order);
