a10aa996968fa00d1951ad019ccd560837fa402e
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 15:48:30 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/CustomerController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/CustomerController.java
@@ -5,6 +5,7 @@ import javax.security.auth.login.LoginException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -39,7 +40,7 @@ public class CustomerController {
 	}
 	
 	
-	
+	@DeleteMapping
 	public ResponseEntity<Customer> veiwCustomerByIdHandler(@RequestParam Integer customerId,@RequestParam String key) throws CustomerException, LoginException  {
 
 	
