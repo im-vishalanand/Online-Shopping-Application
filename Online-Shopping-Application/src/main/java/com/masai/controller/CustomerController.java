50618c32f36c6d6313fc571d886150a2e76ff46f
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 15:34:06 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/CustomerController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/CustomerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class CustomerController {
 		return new ResponseEntity<>(savedcustomer, HttpStatus.CREATED);
 	}
 	
-	
+	@PutMapping
 	public ResponseEntity<Customer> updateCoustomerHandler( @RequestBody Customer customer)  {
 
 	
