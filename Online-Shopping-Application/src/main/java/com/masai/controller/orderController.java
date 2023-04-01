56e3be823210c93f09e1be27c98e33c44738c071
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 14:50:05 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,9 @@ public class orderController {
 		 
 	 }
 	
+	
+	
+	@PutMapping
 	 public ResponseEntity<Orders> updateOrderHandler(@RequestBody Orders order){
 
 	 
