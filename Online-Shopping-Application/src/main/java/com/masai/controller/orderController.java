6749f1d983c2acec36e75ebcdd954c57dee3425b
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 14:44:14 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
@@ -1,7 +1,6 @@
 package com.masai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.masai.model.Orders;
 import com.masai.service.OrderService;
 
 @RestController
@@ -20,9 +20,9 @@ public class orderController {
 	
 	
 	@PostMapping
-	 public ResponseEntity<Order> addOrderHandler(@RequestBody Order order){
+	 public ResponseEntity<Orders> addOrderHandler(@RequestBody Orders order){
 		 
-		 Order addedOrder = orderService.addOrder(order);
+		 Orders addedOrder = orderService.addOrder(order);
 		 
 		 return new ResponseEntity<>(addedOrder, HttpStatus.CREATED);
 		 
