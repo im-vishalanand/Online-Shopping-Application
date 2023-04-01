1e7f335af2d28981b21bcd08bb70f0b866a351ae
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 14:58:08 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/ProductController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/ProductController.java
@@ -1,9 +1,15 @@
 package com.masai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.masai.exception.ProductException;
+import com.masai.model.Product;
 import com.masai.service.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -14,6 +20,10 @@ public class ProductController {
 	
 	
 	public ResponseEntity<Product> addProductHandler(@Valid @RequestBody Product product) throws ProductException{
+		
+		
+		
+		
 		
 		
 	}
