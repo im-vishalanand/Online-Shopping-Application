ed79aa8e28f9be29be25849ad810b2a2bd918693
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 14:57:08 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/ProductController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.masai.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.service.ProductService;
 
 @RestController
 public class ProductController {
@@ -9,3 +12,4 @@ public class ProductController {
 	@Autowired
 	private ProductService prodServ;
 }
+	
