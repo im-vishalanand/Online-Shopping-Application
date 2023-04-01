b327df5d9de48d93228c379a264a3e397189faab
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 15:02:20 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/ProductController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.masai.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,10 +27,7 @@ public class ProductController {
 		
 		Product addProd = proService.addProduct(product);
 		
-		return new ResponseEntity<Product>(addProd, HttpStatus.CREATED);
-		
-		
-		
+		return new ResponseEntity<Product>(addProd, HttpStatus.CREATED);	
 		
 	}
 	
