223a6ec3884fc3804b72ee3f606ca52349249fb1
mdhumairraza <mdhumairraza@yahoo.com>
2023-04-01 14:52:23 +0530

    day-5

--- Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
+++ Online-Shopping-Application/src/main/java/com/masai/controller/orderController.java
@@ -3,6 +3,7 @@ package com.masai.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,7 +44,7 @@ public class orderController {
 	 
 	 
 	 
-	 
+	 @DeleteMapping
 	 public ResponseEntity<Orders> removeOrderHandler(Orders order){
 		 
 		 Orders orderRemove = orderService.removeOrder(order);
